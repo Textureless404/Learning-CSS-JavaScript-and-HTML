83c8c59f5c344f8390f0dd581b52081f96df3145
MissingTextures404 <153525169+MissingTextures404@users.noreply.github.com>
2025-10-02 15:52:17 +0100

    Initial commit
    putting the stuff i have done so far into github

---
+++ README.md
@@ -0,0 +1,2 @@
+# Learning CSS-JavaScript-and-HTML
+learning the stuff to make a wbesite in uni
---
+++ TestScript.js
@@ -0,0 +1,6 @@
+console.log("Hello person looking at the console");
+
+function changeStylesheet(sheet) 
+{
+    document.getElementById("stylesheet").setAttribute("href", sheet);
+}
